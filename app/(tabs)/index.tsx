bb95daa661cbac8a5da41c2cebff64a1199783b6
Puskar-Roy <puskarroy600@gmail.com>
2025-03-20 18:39:45 +0530

    fix : navbar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,16 @@
 import { Image, StyleSheet, Platform, View, Text } from "react-native";
 import { Link } from "expo-router";
 import Header from "@/components/home/Header";
-import Sidebar from "@/components/home/Sidebar";
 
 
 export default function HomeScreen() {
   return (
-    <View className='text-center min-h-screen flex py-5 items-center'>
-      <Header/>
-      <Text>
-        <Link href={'/login'}> Login</Link>
+    <View className="text-center min-h-screen flex py-5 items-center z-1">
+      <Header />
+      <Text className="mt-10">
+        <Link href={"/login"}> Login</Link>
       </Text>
+      
     </View>
   );
 }
--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -3,7 +3,10 @@ import { View, Text, Animated, TouchableOpacity } from "react-native";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import Entypo from "@expo/vector-icons/Entypo";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import Feather from "@expo/vector-icons/Feather";
+import AntDesign from "@expo/vector-icons/AntDesign";
+
 import { Image } from "react-native";
 const Header = () => {
   const [toggleSide, setToggleSide] = useState(false);
@@ -43,27 +46,56 @@ const Header = () => {
       <Animated.View
         style={{
           transform: [{ translateX: slideAnim }],
+          zIndex: 10, 
+         
         }}
-        className="absolute top-[7rem] left-[-19px] h-screen w-[22rem]  p-7"
+        className="absolute bg-white rounded-xl  z-2 top-[8rem] left-[-19px] h-screen w-[24rem]  p-9"
       >
+        
         <Text className="text-3xl font-semibold">Prof. Sumit Gupta</Text>
         <Text className="text-base text-[#9ea8b8] font- mt-8">MENU</Text>
+
         <View className="px-5 mt-8">
-          <View className="flex flex-row items-center gap-5">
-            <MaterialCommunityIcons
-              name="view-dashboard-outline"
-              size={23}
-              color="#667085"
-            />
-            <Text className="text-xl font-semibold text-[#344054]">
-              Dashboard
+          <View className="flex flex-row items-center justify-between">
+            <View className="flex flex-row items-center gap-3">
+              <MaterialCommunityIcons
+                name="view-dashboard-outline"
+                size={23}
+                color="#667085"
+              />
+              <Text className="text-xl font-normal text-[#344054]">
+                Dashboard
+              </Text>
+            </View>
+            <AntDesign name="down" size={18} color="black" />
+          </View>
+          <View className="flex flex-row items-center mt-9 gap-3">
+            <Feather name="calendar" size={23} color="#667085" />
+            <Text className="text-xl font-normal text-[#344054]">Calendar</Text>
+          </View>
+          <View className="flex flex-row items-center mt-9 gap-3">
+            <FontAwesome6 name="user-circle" size={23} color="#667085" />
+            <Text className="text-xl font-normal text-[#344054]">
+              User Profile
             </Text>
           </View>
-          <View className="flex flex-row items-center mt-6 gap-5">
-            <Feather name="calendar" size={23} color="#667085" />
-            <Text className="text-xl font-semibold text-[#344054]">
-              Calendar
-            </Text>
+
+          <View className="flex flex-row items-center mt-9 justify-between">
+            <View className="flex flex-row items-center gap-3">
+              <FontAwesome6 name="wpforms" size={23} color="#667085" />
+              <Text className="text-xl font-normal text-[#344054]">Forms</Text>
+            </View>
+            <AntDesign name="down" size={18} color="black" />
+          </View>
+
+          <View className="flex flex-row items-center mt-9 justify-between">
+            <View className="flex flex-row items-center gap-3">
+              <FontAwesome6 name="calculator" size={23} color="#667085" />
+              <Text className="text-xl font-normal text-[#344054]">
+                Routine
+              </Text>
+            </View>
+            <AntDesign name="down" size={18} color="black" />
           </View>
         </View>
       </Animated.View>
